af891c671f3535f0833942a2c085ae5a260cace1
rekart0 <38154530+rekart0@users.noreply.github.com>
2018-04-07 23:36:11 +0300

    added random spawn

--- Assets/_Scripts/Fox.cs
+++ Assets/_Scripts/Fox.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class Fox : Singleton<Fox>
-{
-    
+{    
     public event Action OnFoxKilled;
+
+    [SerializeField] private List<Transform> _startPositions;
 
     public Transform target;
     public NavMeshSurface foxSurface;
     private NavMeshAgent agent;
+    private int _lastSpawnPointIndex = -1;
+    private Vector3 _defaultStartPosition;
 
-    private Vector3 startingPoint;
+
     void Start()
     {
+        if(_startPositions == null || _startPositions.Count == 0)
+        {
+            _defaultStartPosition = transform.position;
+        }
         agent = GetComponent<NavMeshAgent>();
-        startingPoint = transform.position;
     }
 
     public void StartWalking()
@@ -28,7 +34,7 @@ public class Fox : Singleton<Fox>
 
     public void StopWalking()
     {
-        agent.Warp(startingPoint);
+        agent.Warp(GetRandomPosition());
         agent.isStopped = true;
     }
     
@@ -41,13 +47,33 @@ public class Fox : Singleton<Fox>
         }
 
         DeathStrandingManager.instance.SetKillingPoint(transform.position - Vector3.up);
-        agent.Warp(startingPoint);
+        agent.Warp(GetRandomPosition());
         agent.SetDestination(target.position);
     }
 [EditorButton]
     public void ResetDestination()
     {
         agent.SetDestination(target.position);
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        if(_startPositions.Count == 0)
+        {
+            return _defaultStartPosition;
+        }
+        else
+        {
+            int index = _lastSpawnPointIndex;
+            do
+            {
+                index = UnityEngine.Random.Range(0, _startPositions.Count);
+            }
+            while (_lastSpawnPointIndex == index && _startPositions.Count > 1);
+            _lastSpawnPointIndex = index;
+
+            return _startPositions[index].position;
+        }        
     }
 
     void Update()
