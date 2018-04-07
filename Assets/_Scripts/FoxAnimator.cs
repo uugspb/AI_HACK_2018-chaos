99d7606e2fac53b68449bce6e5b104aee9ff8b77
Alexander <psp_4ever@mail.ru>
2018-04-07 17:20:06 +0300

    FoxAnimator

---
+++ Assets/_Scripts/FoxAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FoxAnimator : MonoBehaviour {
+	public bool crouch;
+	NavMeshAgent m_agent;
+	Animator m_animator;
+	// Use this for initialization
+	void Start () {
+		m_agent = GetComponentInParent<NavMeshAgent> ();
+		m_animator = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		m_animator.SetFloat ("speed", m_agent.velocity.magnitude);
+		m_animator.SetBool ("crouching", crouch);
+	}
+}
