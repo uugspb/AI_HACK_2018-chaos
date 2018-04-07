10f5f71f4d3e4094053ed34af56a18b3dbefbf49
Syjgin <gruz103@gmail.com>
2018-04-07 16:19:10 +0300

    editor scene & basic buttons added

---
+++ Assets/_Scripts/SecretBaseEditor/OutputLevelConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutputLevelConfig
+{
+     public List<Vector2> CameraCoordinates = new List<Vector2>();
+     public List<Vector2> SentinelCoordinates = new List<Vector2>();
+     
+     public int getCamerasCount()
+     {
+          return CameraCoordinates.Count;
+     }
+
+     public int getSentinelCount()
+     {
+          return SentinelCoordinates.Count;
+     }
+
+     public void addCamera(Vector2 coordinates)
+     {
+          CameraCoordinates.Add(coordinates);
+     }
+
+     public void addSentinel(Vector2 coordinates)
+     {
+          SentinelCoordinates.Add(coordinates);
+     }
+}
