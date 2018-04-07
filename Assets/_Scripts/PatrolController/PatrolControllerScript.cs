640099c6331e571add4984975f411984a277a317
Polyakov.Victor <kre-sod@mail.ru>
2018-04-07 23:21:52 +0300

    Убрал в комментарии Alarm

--- Assets/_Scripts/PatrolController/PatrolControllerScript.cs
+++ Assets/_Scripts/PatrolController/PatrolControllerScript.cs
@@ -6,37 +6,37 @@ public class PatrolControllerScript : MonoBehaviour {
 
 	[SerializeField] private List<FovSource> _fovSources;
 
-	[SerializeField] private float _timeToWait = 1;
+	[SerializeField] private float _timeToWaitBeforeKill = 1f;
 
 	// Use this for initialization
 	void Start () {
-		foreach(var source in _fovSources)
-		{
-			source.OnOpponentVisible += OnOpponentVisibleTest;
-		}
+		
 	}
 
 	private void OnOpponentVisibleTest(FovTarget arg1, FovTarget arg2)
 	{
 		Debug.LogWarning(arg1.name + " can see " + arg2.name);
-		StartCoroutine (ReadyToFire(arg2));
+		if (arg2.gameObject.GetComponent<Fox> () != null)
+			StartCoroutine (ReadyToFire (arg2));
 	}
 
 	// ждет некоторое время, после чего стреляет
 	private IEnumerator ReadyToFire(FovTarget arg2)
 	{
-		yield return new WaitForSeconds (_timeToWait);
+		yield return new WaitForSeconds (_timeToWaitBeforeKill);
 		OpenFire (arg2);
 	}
 		
 	private void OpenFire (FovTarget arg2)
 	{
-
-		//arg2.gameObject.GetComponent<Fox> ().isDead = true;
+		arg2.gameObject.GetComponent<Fox> ().Kill();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		foreach(var source in _fovSources)
+		{
+			source.OnOpponentVisible += OnOpponentVisibleTest;
+		}
 	}
 }
