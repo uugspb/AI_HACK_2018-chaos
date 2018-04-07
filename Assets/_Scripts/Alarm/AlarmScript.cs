ae7986f622211d4be2af16931ba82ee327e96cf5
Polyakov.Victor <kre-sod@mail.ru>
2018-04-07 18:01:54 +0300

    Сделал скрипт тревоги

---
+++ Assets/_Scripts/Alarm/AlarmScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmScript : MonoBehaviour {
+
+	[SerializeField] private bool _isAlarm;
+	[SerializeField] private float _alarmTime = 10f;
+
+	// Use this for initialization
+	void Start () {
+		_isAlarm = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_isAlarm) 
+		{
+			PanicTime ();
+		}
+	}
+
+	private void PanicTime()
+	{
+		_alarmTime -= Time.timeScale * Time.deltaTime;
+	}
+}
