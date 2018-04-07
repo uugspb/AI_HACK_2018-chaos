6fa4db17df7929fd576881284dd0eda9cb93c94a
rekart0 <38154530+rekart0@users.noreply.github.com>
2018-04-07 17:57:32 +0300

    fov sector ui implemented

---
+++ Assets/_Scripts/FOV/FovSectorUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FovSectorUI : MonoBehaviour {
+
+    [SerializeField] private Image _sectorImage;
+
+    private RectTransform _imageRect;
+
+    public float SectorDegrees;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        _imageRect = _sectorImage.GetComponent<RectTransform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var angles = _imageRect.localEulerAngles;
+        angles.z = SectorDegrees;
+        _imageRect.localEulerAngles = angles;
+
+        _sectorImage.fillAmount = SectorDegrees * 2 / 360;
+	}
+}
