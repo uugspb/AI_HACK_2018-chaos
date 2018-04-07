e57aae70b738caec0cf590c7114d9c7fa76ad4c7
rekart0 <38154530+rekart0@users.noreply.github.com>
2018-04-07 15:22:32 +0300

    added view logic

---
+++ Assets/_Scripts/FOV/FovSource.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class FovSource : MonoBehaviour {
+
+    public event Action<FovTarget, FovTarget> OnOpponentVisible;
+
+    [SerializeField] private FovTarget _me;
+    [SerializeField] private GameObject _eyePoint;
+    private Collider _fovCollider;
+
+    private static readonly float _castDistance = 100;
+
+    // Use this for initialization
+    void Start () {
+        _fovCollider = GetComponent<Collider>();
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        var opponent = other.GetComponent<FovTarget>();
+        if (opponent != null && opponent.Type != _me.Type)
+        {            
+            var opponentPosition = other.transform.position;
+            var direction = (opponentPosition - _eyePoint.transform.position).normalized;
+
+            RaycastHit hit;
+            if(Physics.Raycast(_eyePoint.transform.position, direction,out hit, _castDistance))
+            {
+                var detectedOpponent = hit.collider.GetComponent<FovTarget>();
+                if(detectedOpponent != null && detectedOpponent.Type != _me.Type)
+                {
+                    if(OnOpponentVisible != null)
+                    {
+                        OnOpponentVisible.Invoke(_me, detectedOpponent);
+                    }                    
+                }
+            }
+        }
+    }
+}
