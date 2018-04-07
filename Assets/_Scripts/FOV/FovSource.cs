f87726d7883b1488713757850106a5042057ac9c
rekart0 <38154530+rekart0@users.noreply.github.com>
2018-04-07 17:29:23 +0300

    done fov

--- Assets/_Scripts/FOV/FovSource.cs
+++ Assets/_Scripts/FOV/FovSource.cs
@@ -11,38 +11,38 @@ public class FovSource : MonoBehaviour
     public event Action<FovTarget, FovTarget> OnOpponentVisible;
 
     [SerializeField] private FovTarget _me;
-    [SerializeField] private GameObject _eyePoint;
+    [SerializeField] private float _fovAngle;
     private Collider _fovCollider;
 
-    private static readonly string _agentLayer = "Default";
-    private static int _agentLayerId = -1;
 
     // Use this for initialization
     void Start()
     {
         _fovCollider = GetComponent<Collider>();
-        if (_agentLayerId < 0)
-        {
-            _agentLayerId = LayerMask.NameToLayer(_agentLayer);
-        }
     }
 
     private void OnTriggerStay(Collider other)
     {
         var opponent = other.GetComponent<FovTarget>();
         if (opponent != null && opponent.Type != _me.Type)
-        {
-            var opponentPosition = other.transform.position;
-            var direction = (opponentPosition - _eyePoint.transform.position).normalized;
-            var distance = 100f;// (opponentPosition - _eyePoint.transform.position).magnitude;
+        {        
+            var opponentPosition = opponent.transform.position;
+            var direction = (opponentPosition - transform.position).normalized;
+            var distance =  (opponentPosition - transform.position).magnitude;
 
-            _gizmosP1 = _eyePoint.transform.position;
+            float angle = Vector3.Angle(transform.forward, direction);
+            if (Mathf.Abs(angle) >= _fovAngle)
+            {
+                return;
+            }
+
+            _gizmosP1 = transform.position;
             _gizmosP2 = direction;
             _gizmosP3 = distance;
 
             RaycastHit hit;
 
-            if (Physics.Raycast(_eyePoint.transform.position, direction, out hit, distance, 1))
+            if (Physics.Raycast(transform.position, direction, out hit, distance, 1))
             {
                 var detectedOpponent = hit.collider.GetComponent<FovTarget>();
                 if (detectedOpponent != null && detectedOpponent.Type != _me.Type)
@@ -59,11 +59,14 @@ public class FovSource : MonoBehaviour
 
     private Vector3 _gizmosP1;
     private Vector3 _gizmosP2;
-    private float _gizmosP3;
+    private float   _gizmosP3;
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position +  transform.forward);
+
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(_gizmosP1, _gizmosP2 * _gizmosP3);
+        Gizmos.DrawLine(_gizmosP1, _gizmosP1 + _gizmosP2 * _gizmosP3);
         _gizmosP1 = Vector3.zero;
         _gizmosP2 = Vector3.zero;
     }
