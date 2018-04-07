30fd0417471f7e29da0cd2e4b6d365415441c791
Alexander <psp_4ever@mail.ru>
2018-04-07 21:11:22 +0300

    Chickens

---
+++ Assets/_Scripts/ChickenManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenManager : Singleton<ChickenManager> {
+	public List<Transform> waypoints;
+
+	void OnDrawGizmos () {
+		foreach (var waypoint in waypoints) {
+			Gizmos.DrawSphere (waypoint.position, 0.25f);
+		}
+	}
+}
