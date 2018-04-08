d6230e5b43fd29e4090925d37a46aa73b64c6576
Alexander <psp_4ever@mail.ru>
2018-04-08 06:44:37 +0300

    fox animator bug in logs

--- Assets/_Scripts/FoxAnimator.cs
+++ Assets/_Scripts/FoxAnimator.cs
@@ -8,51 +8,42 @@ public class FoxAnimator : MonoBehaviour {
 	NavMeshAgent m_agent;
 	Animator m_animator;
 
-    public AudioSource walk;
+	public AudioSource walk;
 	// Use this for initialization
 	void Start () {
 		m_agent = GetComponentInParent<NavMeshAgent> ();
 		m_animator = GetComponent<Animator> ();
 	}
 
-    public void ResetAnim()
-    {
-        m_animator.gameObject.SetActive(false);
-        m_animator.gameObject.SetActive(true);
-    }
+	public void ResetAnim () {
+		m_animator.gameObject.SetActive (false);
+		m_animator.gameObject.SetActive (true);
+	}
 
-    // Update is called once per frame
-    void Update () {
+	// Update is called once per frame
+	void Update () {
 		if (m_agent != null) {
 			m_animator.SetFloat ("speed", m_agent.velocity.magnitude);
 		}
 		crouch = DeathStrandingManager.instance != null && DeathStrandingManager.instance.IsCloseToKillingPoint (transform.position);
 		m_animator.SetBool ("crouching", crouch);
 
+		if (Fox.instance != null) {
+			if (!Fox.instance.IsKillInProgress) {
+				if (!crouch && !walk.isPlaying && GameManager.instance.mode == GameMode.play) {
+					walk.Play ();
+				} else if (crouch && walk.isPlaying) {
+					walk.Stop ();
+				}
+			} else {
+				if (Fox.instance.PlayKillAnim) {
+					Fox.instance.PlayKillAnim = false;
+					m_animator.SetTrigger ("die");
+					m_animator.SetBool ("crouching", false);
+					walk.Stop ();
+				}
+			}
+		}
 
-
-        if(!Fox.instance.IsKillInProgress)
-        {
-            if (!crouch && !walk.isPlaying && GameManager.instance.mode == GameMode.play)
-            {
-                walk.Play();
-            }
-            else if (crouch && walk.isPlaying)
-            {
-                walk.Stop();
-            }
-        }
-        else
-        {
-            if (Fox.instance.PlayKillAnim)
-            {
-                Fox.instance.PlayKillAnim = false;
-                m_animator.SetTrigger("die");
-                m_animator.SetBool("crouching", false);
-                walk.Stop();
-            }
-        }
-
-        
 	}
 }
